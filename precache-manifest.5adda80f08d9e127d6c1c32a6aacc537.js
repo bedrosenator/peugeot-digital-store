d1e4f5746605d3a4f3c78b301b20db8c72d3960b
bedrosenator <bedrosenator@gmail.com>
2020-07-01 15:52:26 +0300

    GH pages branch added

---
+++ precache-manifest.5adda80f08d9e127d6c1c32a6aacc537.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fafd66a713317a790a59d513812b8718",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ae670eabc05729248b9c",
+    "url": "/static/css/main.85ca5bce.chunk.css"
+  },
+  {
+    "revision": "ea97461c3fa5d83d6c8f",
+    "url": "/static/js/2.06c45789.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.06c45789.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ae670eabc05729248b9c",
+    "url": "/static/js/main.1e6dc390.chunk.js"
+  },
+  {
+    "revision": "86828d3b2370c708e279",
+    "url": "/static/js/runtime-main.e112d529.js"
+  },
+  {
+    "revision": "32f455d0a3b677ca62dfc6f2f7d5d00a",
+    "url": "/static/media/arrow_back.32f455d0.svg"
+  },
+  {
+    "revision": "19397e204b2ccb5e55f9122d0c9abce3",
+    "url": "/static/media/arrow_forward.19397e20.svg"
+  },
+  {
+    "revision": "033a0393c50ab5f838cd3158f571ad3e",
+    "url": "/static/media/error.033a0393.svg"
+  },
+  {
+    "revision": "5f59b2ff4bebc52ba1f7046757f21090",
+    "url": "/static/media/success.5f59b2ff.svg"
+  }
+]);
